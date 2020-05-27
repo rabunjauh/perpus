2ae0b75e5e0a6c5e9fa7ef77bdba2cb6db56531a
rabunjauh <mustafa.visionet@gmail.com>
2020-05-27 16:37:51 +0700

    coba form tambah data buku pakai modal

--- application/views/contents/view_data_buku.php
+++ application/views/contents/view_data_buku.php
@@ -2,7 +2,7 @@
 	<div class="card-header py-3">
 		<?php
 			$attributes = array('class' => 'd-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search');
-			form_open(base_url('data_buku/search_buku'), $attributes);
+			echo form_open(base_url('data_buku/search_buku'), $attributes);
 		?>		
 			<div class="form-group">
 				<label>Cari Berdasarkan :</label>
